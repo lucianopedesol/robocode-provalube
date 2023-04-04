29640b752544b076dc027a5b8a140b7e31ed209c
Luciano Pedesol <60025358+lucianopedesol@users.noreply.github.com>
2023-04-04 12:32:07 -0300

    Update ProvaLube.java

--- ProvaLube.java
+++ ProvaLube.java
@@ -17,22 +17,29 @@ public class ProvaLube  extends AdvancedRobot {
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
+	// Ativa o radar para mirar em quem for encontrando pelo caminho 
         double radarTurn = getHeadingRadians() + e.getBearingRadians() - getRadarHeadingRadians();
         double gunTurn = getHeadingRadians() + e.getBearingRadians() - getGunHeadingRadians();
         setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
         setTurnGunRightRadians(Utils.normalRelativeAngle(gunTurn)); 
-	double enemyBearing = e.getBearingRadians();
-	double enemyHeading = e.getHeadingRadians();
-	double bearingFromGun = Utils.normalRelativeAngle(enemyBearing - getGunHeadingRadians() + enemyHeading);
 	    
+	//Ângulo em radianos do robô adversário em relação ao seu.
+	double enemyBearing = e.getBearingRadians();
+	//Retorna o ângulo em radiaons do adversário em relação a tela.
+	double enemyHeading = e.getHeadingRadians();
+	// Calcula o ângulo necessário para girar a arma e mirar no inimigo
+	double bearingFromGun = Utils.normalRelativeAngle(enemyBearing - getGunHeadingRadians() + enemyHeading);
+	// Se o angulo for menor que 3 atira com mais força 
     	if (Math.abs(bearingFromGun) < Math.toRadians(3)) {
 	    setFire(3);
     	} 
+	// Se não, fica atirando normalmente
         fire(1);
     }
 	
+    //Fugir para alguma direção aleatoria quando for atingido.
     public void onHitByBullet(HitByBulletEvent e) {
-        // inverte a direção do movimento
+        // Muda a direção do movimento atual
         moveDirection = -moveDirection; 
 
         // Vira para um lado aleatório
