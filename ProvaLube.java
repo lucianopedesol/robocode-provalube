75ea9ac6138179b5b330c8afa99d4a2239783e37
Luciano Pedesol <lucianopedesol@hotmail.com>
2023-04-04 00:03:52 -0300

    robocode

---
+++ ProvaLube.java
@@ -0,0 +1,51 @@
+package sample;
+import robocode.*;
+import robocode.util.*;
+
+public class ProvaLube  extends AdvancedRobot {
+     private int moveDirection = 1;
+
+    public void run() {
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        
+        while (true) {
+            setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+            setAhead(100);
+            execute();
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double radarTurn = getHeadingRadians() + e.getBearingRadians() - getRadarHeadingRadians();
+        double gunTurn = getHeadingRadians() + e.getBearingRadians() - getGunHeadingRadians();
+        setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
+        setTurnGunRightRadians(Utils.normalRelativeAngle(gunTurn)); 
+		double enemyBearing = e.getBearingRadians();
+	    double enemyHeading = e.getHeadingRadians();
+	    double bearingFromGun = Utils.normalRelativeAngle(enemyBearing - getGunHeadingRadians() + enemyHeading);
+	    
+	    if (Math.abs(bearingFromGun) < Math.toRadians(3)) {
+	        setFire(3);
+	    } 
+        fire(1);
+	 
+    }
+	
+    public void onHitByBullet(HitByBulletEvent e) {
+        moveDirection = -moveDirection; // inverte a direção do movimento
+
+        // Vira para um lado aleatório
+        if (Math.random() > 0.5) {
+            setTurnRight(90);
+        } else {
+            setTurnLeft(90);
+        }
+
+        // Move para trás
+        setBack(100);
+        execute();
+    }
+	
+ 
+}
